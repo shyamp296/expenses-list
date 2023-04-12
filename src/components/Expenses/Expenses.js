55e9313a72a4aab1a9dc68d77b51b11166e06257
shyamp296 <shyamdadhaniya29@gmail.com>
2023-04-12 17:37:09 +0530

    3rd

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,30 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
-import "./Expenses.css";
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
-  console.log("🚀 ~ file: Expenses.js:9 ~ Expenses ~ props:", props);
   return (
     <div>
-      <Card className="expenses">
-         <ExpensesFilter
+      <Card className='expenses'>
+        <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
-        {props.items.map((item) => (
+        {props.items.map((expense) => (
           <ExpenseItem
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
           />
         ))}
       </Card>
