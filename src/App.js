b16d9fe5b93a5b1658494d85500fd6933d33d48d
shyamp296 <shyamdadhaniya29@gmail.com>
2023-04-12 16:01:03 +0530

    2nd commmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
 
-import NewExpense from "./components/NewExpense/NewExpense";
-import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense"
+import Expenses from "./components/Expenses/Expenses"
 
 const App = () => {
   const dummy_expenses = [];
 
-  const [expenses, setExpenses] = useState(dummy_expenses);
+  const [expenses, setExpenses] = useState(dummy_expenses)
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
+      return [expense, ...prevExpenses]
     });
   };
-
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
 
   return (
     <div>
